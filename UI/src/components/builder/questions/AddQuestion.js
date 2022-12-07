7b2938cae522ab0af92a34aad5a48b6238ce567e
Mouaiad Hejazi <77430445+moayead2009@users.noreply.github.com>
2022-12-06 22:24:13 -0500

    Add files via upload

--- UI/src/components/builder/questions/AddQuestion.js
+++ UI/src/components/builder/questions/AddQuestion.js
@@ -3,7 +3,7 @@ import useStateContext from "../../../hooks/useStateContext";
 import { createAPIEndpoint, ENDPOINTS } from "../../../api";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { Card, CardContent, Grid, TextField, Button, Typography } from "@mui/material";
+import { Card, CardContent, Grid, TextField, Button, Typography, Select, MenuItem, FormControl, InputLabel } from "@mui/material";
 import { Box } from "@mui/system";
 import { useParams } from "react-router-dom";
 
@@ -11,6 +11,9 @@ export default function AddQuestion() {
   const [values, setValues] = useState({
     body: "",
     type: "",
+    questionType: "",
+    choicesQuestion: [],
+    mous: [],
   });
 
   const [errors, setErrors] = useState({});
@@ -18,6 +21,21 @@ export default function AddQuestion() {
   const { id } = useParams();
   const { state } = useStateContext();
 
+  const addInputField = (event) => {
+    setValues({
+      ...values,
+      mous: [...values.mous, { value: "" }],
+    });
+    event.preventDefault();
+  };
+  const deleteInputField = (index) => {
+    const list = [...values.mous];
+    list.splice(index, 1);
+    setValues({
+      ...values,
+      mous: list,
+    });
+  };
   const validate = () => {
     let temp = {};
     temp.body = values.body ? "" : "This field is required.";
@@ -28,25 +46,37 @@ export default function AddQuestion() {
     return Object.values(temp).every((x) => x === "");
   };
 
+  // const handleSubmit = (e) => {
+  //   console.log(values);
+  //   e.preventDefault();
+  //   if (validate()) {
+  //     if (id) {
+  //       createAPIEndpoint(ENDPOINTS.questions)
+  //         .put(id, values)
+  //         .then((res) => {
+  //           navigate("/builder/questions");
+  //         })
+  //         .catch((err) => console.log(err));
+  //     } else {
+  //       createAPIEndpoint(ENDPOINTS.questions)
+  //         .post(values)
+  //         .then((res) => {
+  //           navigate("/builder/questions");
+  //         })
+  //         .catch((err) => console.log(err));
+  //     }
+  //   }
+  // };
+
   const handleSubmit = (e) => {
+    console.log(values);
     e.preventDefault();
-    if (validate()) {
-      if (id) {
-        createAPIEndpoint(ENDPOINTS.questions)
-          .put(id, values)
-          .then((res) => {
-            navigate("/builder/questions");
-          })
-          .catch((err) => console.log(err));
-      } else {
-        createAPIEndpoint(ENDPOINTS.questions)
-          .post(values)
-          .then((res) => {
-            navigate("/builder/questions");
-          })
-          .catch((err) => console.log(err));
-      }
-    }
+    createAPIEndpoint(ENDPOINTS.questions)
+      .post(values)
+      .then((res) => {
+        navigate("/builder/questions");
+      })
+      .catch((err) => console.log(err));
   };
 
   useEffect(() => {
@@ -89,24 +119,59 @@ export default function AddQuestion() {
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  label="Type"
-                  name="type"
-                  onChange={(e) =>
-                    setValues({
-                      ...values,
-                      type: e.target.value,
-                    })
-                  }{...(errors.type && {
-                    error: true,
-                    helperText: errors.type,
-                  })}
-                  required
-                  value={values.type}
-                  variant="outlined"
-                />
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Question Type</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={values.questionType}
+                    label="Question Type"
+                    onChange={(e) =>
+                      setValues({
+                        ...values,
+                        questionType: e.target.value,
+                      })
+                    }
+                  >
+                    <MenuItem value={"Text"}>{"Text"}</MenuItem>
+                    <MenuItem value={"CheckBox"}>{"CheckBox"}</MenuItem>
+                    <MenuItem value={"Radio"}>{"Radio"}</MenuItem>
+                  </Select>
+                </FormControl>
               </Grid>
+              {(values.questionType === "CheckBox" || values.questionType === "Radio") && (
+
+                <Grid item xs={12}>
+                  <button onClick={addInputField}>Add Choices</button>
+                  {values.mous.map((x, i) => {
+                    return (
+                      <Grid item xs={12} key={i}>
+                        <TextField
+                          key={i}
+                          id="preference"
+                          label={`Choice ${i + 1}`}
+                          name="points"
+                          onChange={(e) =>
+                            setValues({
+                              ...values,
+                              mous: values.mous.map((x, j) =>
+                                i === j ? { value: e.target.value } : x
+                              )
+                            })
+                          }{...(errors.points && {
+                            error: true,
+                            helperText: errors.points,
+                          })}
+
+                          value={x.value}
+                          margin="normal"
+                        />
+                        <Button onClick={deleteInputField}>Delete</Button>
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              )}
               <Grid item xs={12}>
                 <Button
                   color="primary"
